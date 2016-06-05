6cbaf8fcbe329480dac1bee13c61bef57cd2b42c
Artur Klajnerok <a.klajnerok@gmail.com>
2016-06-05 04:09:12 -0700

    Bumped assets cache version

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-var cacheName = 'weatherPWA-step-5-1';
+// Add dataCacheName here
+var cacheName = 'weatherPWA-step-6-1';
 var filesToCache = [
   '/',
   '/index.html',
@@ -8,8 +9,8 @@ var filesToCache = [
   '/images/cloudy-scattered-showers.png',
   '/images/cloudy.png',
   '/images/fog.png',
-  '/images/ic\_add\_white\_24px.svg',
-  '/images/ic\_refresh\_white\_24px.svg',
+  '/images/ic_add_white_24px.svg',
+  '/images/ic_refresh_white_24px.svg',
   '/images/partly-cloudy.png',
   '/images/rain.png',
   '/images/scattered-showers.png',
@@ -23,7 +24,7 @@ self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
+      console.log('[ServiceWorker] Caching App Shell');
       return cache.addAll(filesToCache);
     })
   );
@@ -45,6 +46,7 @@ self.addEventListener('activate', function(e) {
 
 self.addEventListener('fetch', function(e) {
   console.log('[ServiceWorker] Fetch', e.request.url);
+  // Check if requested URL is for the data service here
   e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
