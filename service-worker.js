9b984cf88b24b3c976499402ddbdd9a45c27b2ba
Artur Klajnerok <a.klajnerok@gmail.com>
2016-06-05 04:08:21 -0700

    Intercept fetch and load assets from cache

--- service-worker.js
+++ service-worker.js
@@ -42,3 +42,12 @@ self.addEventListener('activate', function(e) {
     })
   );
 });
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
