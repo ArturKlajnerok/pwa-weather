bb06d8614d5a36727aeaa757bd99fb0358d74d41
Artur Klajnerok <a.klajnerok@gmail.com>
2016-08-09 11:20:54 -0700

    Handle notification clicks push notifications

--- service-worker-push-notifications.js
+++ service-worker-push-notifications.js
@@ -42,3 +42,25 @@ self.addEventListener('push', function(event) {
       tag: 'my-tag'
     }));
 });
+
+self.addEventListener('notificationclick', function(event) {
+    console.log('Notification click: tag ', event.notification.tag);
+    event.notification.close();
+    var url = 'https://www.windfinder.com';
+    event.waitUntil(
+        clients.matchAll({
+            type: 'window'
+        })
+        .then(function(windowClients) {
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            if (clients.openWindow) {
+                return clients.openWindow(url);
+            }
+        })
+    );
+});
